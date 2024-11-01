f491a673e572815114beccaa9df343579ae70fec
Jorrmungandr <edgar@lunary.ai>
2024-11-01 16:38:13 -0300

    fix: add logs

--- src/modules/upload-document/upload-document.service.ts
+++ src/modules/upload-document/upload-document.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 
 import { S3Service } from 'src/services/s3.service';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 @Injectable()
 export class UploadDocumentService {
   constructor(private readonly s3: S3Service) {}
 
   async uploadDocument(file: Express.Multer.File): Promise<void> {
-    const result = await this.s3.uploadFile(file);
+    try {
+      const result = await this.s3.uploadFile(file);
 
-    const fileUrl = await this.s3.getSignedUrl(result.Key);
+      const fileUrl = await this.s3.getSignedUrl(result.Key);
 
-    await axios.post(process.env.LANGFLOW_URL, {
-      input_value: 'message',
-      output_type: 'text',
-      input_type: 'text',
-      tweaks: {
-        'Upload Document Webhook': {
-          url: fileUrl,
+      console.log('make axios post');
+
+      await axios.post(process.env.LANGFLOW_URL, {
+        input_value: 'message',
+        output_type: 'text',
+        input_type: 'text',
+        tweaks: {
+          'Upload Document Webhook': {
+            url: fileUrl,
+          },
         },
-      },
-    });
+      });
+    } catch (err) {
+      console.log('Upload err', err);
+
+      if (err instanceof AxiosError) {
+        console.log(err.response);
+      }
+    }
   }
 }
