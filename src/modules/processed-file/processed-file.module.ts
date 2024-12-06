b22e58ce762e416e14e8c77b773a5eb1a725f032
Jorrmungandr <edgar@lunary.ai>
2024-12-06 13:03:00 -0300

    fix: add s3Service to processedFileModule

--- src/modules/processed-file/processed-file.module.ts
+++ src/modules/processed-file/processed-file.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaService } from 'src/services';
+import { PrismaService, S3Service } from 'src/services';
 
 import { ProcessedFileService } from './processed-file.service';
 import { ProcessedFileController } from './processed-file.controller';
 
 @Module({
   controllers: [ProcessedFileController],
-  providers: [ProcessedFileService, PrismaService],
+  providers: [ProcessedFileService, PrismaService, S3Service],
   exports: [ProcessedFileService],
 })
 export class ProcessedFileModule {}
